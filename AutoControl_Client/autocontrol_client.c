92967e7d3a4338bbefbbd25e514deb0a5f304b84
zs-nemecz <zsuzska.nemecz@gmail.com>
2018-11-18 15:17:43 +0100

    radio buttons for transmission

--- AutoControl_Client/autocontrol_client.c
+++ AutoControl_Client/autocontrol_client.c
@@ -1,6 +1,6 @@
 #include <gtk/gtk.h>
 
-guint8 set_ignition (GtkWidget *widget, gpointer user_data)
+guint8 get_ignition (GtkWidget *widget, gpointer user_data)
 {
     guint8 ignition;
     if (gtk_switch_get_active(widget) == TRUE)
@@ -15,6 +15,51 @@ guint8 set_ignition (GtkWidget *widget, gpointer user_data)
     }
 
     return ignition;
+}
+
+guint8 get_transmission_mode (GtkWidget *widget, gpointer user_data)
+{
+    guint8 t_mode;
+    if(user_data == 0)
+    {
+        if(gtk_toggle_button_get_active(widget) == TRUE)
+        {
+            t_mode = 0;
+            g_print("Parking\n");
+        }
+    }
+    else if (user_data == 1)
+    {
+        if(gtk_toggle_button_get_active(widget) == TRUE)
+        {
+            t_mode = 1;
+            g_print("Neutral\n");
+        }
+
+    }
+    else if (user_data == 2)
+    {
+        if(gtk_toggle_button_get_active(widget) == TRUE)
+        {
+            t_mode = 2;
+            g_print("Backwards\n");
+        }
+    }
+    else if (user_data == 3)
+    {
+        if(gtk_toggle_button_get_active(widget) == TRUE)
+        {
+            t_mode = 3;
+            g_print("Drive\n");
+        }
+    }
+    else
+    {
+        t_mode = 4;
+        g_print("Transmission mode invalid.\n");
+    }
+
+    return t_mode;
 }
 
 gint16 get_angle (GtkWidget *widget, gpointer user_data)
@@ -41,14 +86,12 @@ int main (int   argc, char *argv[])
     GObject *window;
     GObject *ignition_switch;
     GObject *label;
-    GObject *angle_slider;
-    GObject *speed_slider;
-    GtkAdjustment *scale_adjustment;
+    GObject *slider;
+    GtkWidget *gear_button_p;
+    GtkWidget *gear_button_n;
+    GtkWidget *gear_button_r;
+    GtkWidget *gear_button_d;
 
- /*     GObject *speed_slider;
- *     GObject *gear_button;
- *     GObject *direction_toggle;
- */
     GError *error = NULL;
     gtk_init (&argc, &argv);
 
@@ -66,13 +109,24 @@ int main (int   argc, char *argv[])
     g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
     ignition_switch = gtk_builder_get_object (builder, "ignitionSwitch");
-    g_signal_connect (ignition_switch, "notify::active", G_CALLBACK (set_ignition), NULL);
+    g_signal_connect (ignition_switch, "notify::active", G_CALLBACK (get_ignition), NULL);
 
-    angle_slider = gtk_builder_get_object (builder, "angleSlider");
-    g_signal_connect (angle_slider, "value-changed", G_CALLBACK (get_angle), NULL);
+    slider = gtk_builder_get_object (builder, "angleSlider");
+    g_signal_connect (slider, "value-changed", G_CALLBACK (get_angle), NULL);
 
-    speed_slider = gtk_builder_get_object (builder, "speedSlider");
-    g_signal_connect (speed_slider, "value-changed", G_CALLBACK (get_speed), NULL);
+    slider = gtk_builder_get_object (builder, "speedSlider");
+    g_signal_connect (slider, "value-changed", G_CALLBACK (get_speed), NULL);
+
+    gear_button_p = gtk_builder_get_object (builder, "gearP");
+    gear_button_n = gtk_builder_get_object (builder, "gearN");
+    gear_button_r = gtk_builder_get_object (builder, "gearR");
+    gear_button_d = gtk_builder_get_object (builder, "gearD");
+
+    g_signal_connect (gear_button_p, "toggled", G_CALLBACK (get_transmission_mode), 0);
+    g_signal_connect (gear_button_n, "toggled", G_CALLBACK (get_transmission_mode), 1);
+    g_signal_connect (gear_button_r, "toggled", G_CALLBACK (get_transmission_mode), 2);
+    g_signal_connect (gear_button_d, "toggled", G_CALLBACK (get_transmission_mode), 3);
+
 
     label = gtk_builder_get_object(builder, "ignitionLabel");
     label = gtk_builder_get_object(builder, "angleLabel");
@@ -82,5 +136,3 @@ int main (int   argc, char *argv[])
 
     return 0;
 }
-
-
