28b1a8cffed86ed20d8f29f69cf88aad64c594fa
zs-nemecz <zsuzska.nemecz@gmail.com>
2018-11-22 23:12:36 +0100

    header and separate c file for setting buttons

---
+++ AutoControl_Client/auto_settings_gui.h
@@ -0,0 +1,23 @@
+#ifndef AUTO_SETTING_GUI
+#define AUTO_SETTING_GUI
+
+#include <gtk/gtk.h>
+
+struct IndexButtons {
+    GtkWidget *ButtonLeft;
+    GtkWidget *ButtonRight;
+    guint8 *LeftActive;
+    guint8 *RightActive;
+};
+
+guint8 get_ignition (GtkWidget *widget, gpointer user_data);
+
+guint8 get_index (GtkWidget *widget, struct IndexButtons *button_stucture);
+
+guint8 get_transmission_mode (GtkWidget *widget, gpointer user_data);
+
+gint16 get_angle (GtkWidget *widget, gpointer user_data);
+
+guint16 get_speed (GtkWidget *widget, gpointer user_data);
+
+#endif
